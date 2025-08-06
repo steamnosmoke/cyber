246e5a10b967a0911598705253e8e2d298463798
Yuriy <pe4enka055@gmail.com>
2025-08-06 21:24:36 +0300

    Added functions of cart for unlogined user

--- src/app/hooks/useAddToCart.ts
+++ src/app/hooks/useAddToCart.ts
@@ -6,37 +6,49 @@ import DB_URL from "constants/DB_URL";
 import { useAuthStore } from "store/authStore";
 import { TCartItem } from "types/CartTypes";
 
-const getCart = async () => {
+const getUserCart = async (): Promise<TCartItem[]> => {
   const userId: string = useAuthStore.getState().user.firebaseId;
-  const res = await axios.get<Record<string, TCartItem>>(
+  const { data } = await axios.get<Record<string, TCartItem>>(
     `${DB_URL}/users/${userId}/cart.json`
   );
-  const data = res.data;
-  return { data: data ? Object.entries(data) : [], userId };
+  const cartData: TCartItem[] = data
+    ? Object.entries(data).map((el) => {
+        const [key, value] = el;
+        return { ...value, id: Number(key) };
+      })
+    : [];
+  return cartData;
 };
 
-async function addToCart(product: TCartItem) {
-  const { data, userId } = await getCart();
+const getGuestCart = (): TCartItem[] => {
+  const cartData: Record<string,TCartItem> = JSON.parse(localStorage.getItem("guest-cart"));
+  const cart: TCartItem[] = cartData ? Object.values(cartData) : []
+  return cart;
+};
+
+async function addToUserCart(product: TCartItem): Promise<TCartItem> {
+  const userId = useAuthStore.getState().user.firebaseId;
+  const data = await getUserCart();
   const existingEntry = data.find(
-    ([_, value]) =>
-      value.productId === product.productId &&
-      (value.variantId || null) === (product.variantId || null)
+    (item) =>
+      item.productId === product.productId &&
+      (item.variantId || null) === (product.variantId || null)
   );
   if (existingEntry) {
-    const [key, value] = existingEntry;
-    const updatedCount = Number(value.count) + 1;
-    const updatedTotal = Number(value.total) + Number(value.totalPrice);
-    const updatedDiscount = updatedCount * Number(value.discount);
-    const updatedSubTotal = updatedCount * Number(value.price);
+    const updatedCount = Number(existingEntry.count) + 1;
+    const updatedTotal =
+      Number(existingEntry.total) + Number(existingEntry.totalPrice);
+    const updatedDiscount = updatedCount * Number(existingEntry.discount);
+    const updatedSubTotal = updatedCount * Number(existingEntry.price);
     const updatedItem = {
-      ...value,
+      ...existingEntry,
       count: updatedCount,
       total: updatedTotal,
       totalDiscount: updatedDiscount,
       subTotal: updatedSubTotal,
     };
     await axios.patch<Record<string, TCartItem>>(
-      `${DB_URL}/users/${userId}/cart/${key}.json`,
+      `${DB_URL}/users/${userId}/cart/${existingEntry.id}.json`,
       updatedItem
     );
     return updatedItem;
@@ -58,10 +70,63 @@ async function addToCart(product: TCartItem) {
 
     return {
       ...newItem,
-      id: postRes.data.name,
+      id: Number(postRes.data.name),
     };
   }
 }
+
+async function addToGuestCart(product: TCartItem): Promise<TCartItem> {
+  const cartData = getGuestCart();
+  const existingEntry = cartData.find(
+    (item) =>
+      item.productId === product.productId &&
+      (item.variantId || null) === (product.variantId || null)
+  );
+  if (existingEntry) {
+    const updatedCount = Number(existingEntry.count) + 1;
+    const updatedTotal =
+      Number(existingEntry.total) + Number(existingEntry.totalPrice);
+    const updatedDiscount = updatedCount * Number(existingEntry.discount);
+    const updatedSubTotal = updatedCount * Number(existingEntry.price);
+    const updatedItem = {
+      ...existingEntry,
+      count: updatedCount,
+      total: updatedTotal,
+      totalDiscount: updatedDiscount,
+      subTotal: updatedSubTotal,
+    };
+
+    const existingKey = cartData.indexOf(existingEntry);
+    cartData[existingKey] = updatedItem;
+    localStorage.setItem("guest-cart", JSON.stringify(cartData));
+    return updatedItem;
+  } else {
+    const total = Number(product.totalPrice);
+    const totalDiscount = Number(product.discount);
+    const subTotal = Number(product.price);
+    const newItem = {
+      ...product,
+      count: 1,
+      total,
+      totalDiscount,
+      subTotal,
+    };
+    cartData.push(newItem);
+    localStorage.setItem("guest-cart", JSON.stringify(cartData));
+    return {
+      ...newItem,
+      id: cartData.length - 1,
+    };
+  }
+}
+
+const addToCart = async (product: TCartItem) => {
+  const userId = useAuthStore.getState().user.firebaseId;
+  if (userId === "guest") {
+    return addToGuestCart(product);
+  }
+  return addToUserCart(product);
+};
 
 export function useAddToCart() {
   const queryClient = useQueryClient();
