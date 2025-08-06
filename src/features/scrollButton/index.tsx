3efa0edbd5bf9d1d3ad7dd58719e6d8b4197fd91
Yuriy <pe4enka055@gmail.com>
2025-08-06 20:45:51 +0300

    Added button for fast scroll to up

---
+++ src/features/scrollButton/index.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { ArrowUp } from "lucide-react";
+
+export default function ScrollToTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    <button
+      onClick={scrollToTop}
+      className={`fixed bottom-6 right-6 z-50 w-15 h-15 justify-items-center cursor-pointer rounded-full bg-black text-white shadow-lg transition-all duration-200 scrollButton ${
+        isVisible ? "visible opacity-70" : "invisible opacity-0"
+      } hover:opacity-100`}
+      aria-label="Scroll to top"
+    >
+      <ArrowUp size={40} />
+    </button>
+  );
+}
