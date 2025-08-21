3b52b1392bc428c8d19b0b3b0bc7c5335f232e31
Yuriy <pe4enka055@gmail.com>
2025-08-21 10:35:00 +0300

    Minor update

--- src/pages/Wishlist/index.tsx
+++ src/pages/Wishlist/index.tsx
@@ -1,9 +1,9 @@
-import { TProduct } from "@/src/app/types/ProductTypes";
+import { TProduct } from "types/ProductTypes";
 import Products from "components/Products";
-import { usePage } from "hooks/usePage";
+import useGetItems from "hooks/useGetItems";
 
 export default function Wishlist() {
-  const { items: products, status } = usePage<TProduct>("wishlist");
+  const { items: products, status } = useGetItems <TProduct>("wishlist");
   return (
     <section className="wishlist mt-4 flex-grow">
       <div className="container">
