90973a40521dbad91617cefc7fbf9c16e17e37fa
Yuriy <pe4enka055@gmail.com>
2025-08-21 10:36:05 +0300

    Updated structure

--- src/shared/components/ProductCard/index.tsx
+++ src/shared/components/ProductCard/index.tsx
@@ -1,28 +1,24 @@
-import { useCallback } from "react";
+import { useCallback} from "react";
 import { Link } from "react-router";
 
-import { useProductsStore } from "store/productsStore";
-import { usePage } from "hooks/usePage";
-import { useAddToCart } from "hooks/useCart";
-import { useToggleProduct } from "hooks/useWihlist";
-import { TProduct } from "@/src/app/types/ProductTypes";
+import { useProductStore } from "store/productsStore";
+import useGetItems from "hooks/useGetItems";
+import useAddToCart from "hooks/cart/useAddToCart";
+import useAddToWishlist from "hooks/wishlist/addToWishlist";
+import { TProduct } from "types/ProductTypes";
+import { TCartItem } from "types/CartTypes";
 
-import Wishlist from "./components/Wishlist";
-import WishlistAdded from "./components/WishlistAdded";
+import Wishlist from "./images/components/Wishlist";
+import WishlistAdded from "./images/components/WishlistAdded";
 import BlackButton from "buttons/components/BlackButton";
-import { TCartItem } from "@/src/app/types/CartTypes";
 
 export default function Card({ product }: { product: TProduct }) {
-  const setProduct = useProductsStore((state) => state.setProduct);
-  const addToWishlist = useToggleProduct();
+  const setProduct = useProductStore((state) => state.setProduct);
   const addToCart = useAddToCart();
-  const { items: products } = usePage<TProduct>("wishlist");
-  const isLiked = products?.some(
-    (item) =>
-      item.productId === product.productId &&
-      item.objectId === product.objectId &&
-      item.variantId === product.variantId
-  );
+
+  const { items: products } = useGetItems<TProduct>("wishlist");
+  const isLiked = products?.some((item) => item.objectId === product.objectId);
+  const addToWishlist = useAddToWishlist();
 
   const onClickCard = useCallback(() => {
     setProduct(product);
@@ -30,27 +26,17 @@ export default function Card({ product }: { product: TProduct }) {
     window.scrollTo(0, 0);
   }, [setProduct, product]);
 
-  const onAddToCart = useCallback(
-    (product: TProduct) => {
-      const item: TCartItem = {
-        ...product,
-        count: 1,
-        totalDiscount: product.discount,
-        totalPrice: product.price - product.discount,
-        total: product.price - product.discount,
-        subTotal: product.price,
-      };
-      addToCart.mutate(item);
-    },
-    [addToCart, product]
-  );
+  const { items: cart } = useGetItems<TCartItem>("cart");
+  const cartItem = cart?.find((el) => el.objectId === product.objectId);
+  const currentCount = cartItem?.count ?? 0;
+  const isOutOfStock = product.stock <= currentCount;
 
   return (
     <>
       <section className="card w-75 bg-white relative pt-18 px-7.5 pb-6 rounded-2xl shadow-[0_0_0_-5px_rgb(223,223,223)] flex flex-col items-center gap-5 transition-all duration-200 ease-in-out hover:translate-y-[-24px] hover:shadow-[0_20px_24px_-1px_rgb(223,223,223)]">
         <button
           className="add-to-wishlist cursor-pointer absolute top-4 right-4 z-100 group"
-          onClick={() => addToWishlist.mutate(product)}
+          onClick={() => addToWishlist(product)}
         >
           {isLiked ? <WishlistAdded /> : <Wishlist />}
         </button>
@@ -60,9 +46,10 @@ export default function Card({ product }: { product: TProduct }) {
           }${Number(product.memory) === 1 ? "TB" : "GB"}`
             .split(" ")
             .join("_")}
+          onClick={() => onClickCard()}
           className="grid grid-cols-1 grid-rows-[auto_80px_20px] items-center gap-10 cursor-pointer relative"
         >
-          <div className="w-full h-75" onClick={() => onClickCard()}>
+          <div className="w-full h-75">
             <img
               className="w-55 h-[90%] mx-auto"
               src={product.images[0]}
@@ -80,8 +67,14 @@ export default function Card({ product }: { product: TProduct }) {
           </p>
         </Link>
         <BlackButton
-          onClick={() => onAddToCart({ ...product })}
-          children={"Add to cart"}
+          onClick={() => addToCart(product)}
+          children={isOutOfStock ? "Out of stock" : "Add to Cart"}
+          twclass={
+            isOutOfStock
+              ? "!cursor-not-allowed !text-stone-500 !bg-stone-100 !border-stone-300 hover:!bg-stone-100 hover:!text-stone-500 hover:!scale-100"
+              : ""
+          }
+          disabled={isOutOfStock}
         />
       </section>
     </>
