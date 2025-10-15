6d24deb79c09e2a3efae03c6539a70b4a1ed458a
Yuriy <pe4enka055@gmail.com>
2025-10-15 12:00:22 +0300

    Updaet structure and logic  of buttons

--- src/pages/Product/components/Params/components/Buttons.tsx
+++ src/pages/Product/components/Params/components/Buttons.tsx
@@ -1,44 +1,37 @@
-import useGetItems from "hooks/useGetItems";
-import useAddToCart from "hooks/cart/useAddToCart";
-import useAddToWishlist from "hooks/wishlist/addToWishlist";
+import useGetWishlist from "hooks/wishlist/useGetWishlist";
+import useAddToWishlist from "hooks/wishlist/useAddToWishlist";
 
 import { useProductStore } from "store/productsStore";
+import { useAuthStore } from "store/authStore";
 
-import { TProduct } from "types/ProductTypes";
-import { TCartItem } from "types/CartTypes";
+import WishlistAdded from "images/components/WishlistAdded";
+import Wishlist from "images/components/Wishlist";
+import AddToCartButton from "buttons/components/AddToCartButton";
 
-
-import BlackLineButton from "buttons/components/BlackLineButton";
 export default function Buttons() {
-  const { items: wishlist } = useGetItems<TProduct>("wishlist");
-  const { items: cart } = useGetItems<TCartItem>("cart");
+  const userId = useAuthStore((state) => state.firebaseId);
+  const { wishlist } = useGetWishlist(userId);
   const product = useProductStore((state) => state.product);
-  const cartItem = cart.find((el) => el.objectId === product.objectId);
-  const currentCount = cartItem?.count ?? 0;
-  const isOutOfStock = currentCount >= product.stock || product.stock === 0;
-  const addToWishlist = useAddToWishlist();
-  const addToCart = useAddToCart();
+  const addToWishlist = useAddToWishlist(userId);
+
+  const isLiked = wishlist?.some((item) => item.objectId === product.objectId);
+
   return (
-    <div className="buttons flex items-center justify-between w-full ">
-      <BlackLineButton
+    <div className="buttons flex items-center justify-around w-full gap-20 mb-4">
+      <AddToCartButton
+        product={product}
+        className="!w-[452px]"
+      />
+      <button
+        className="add-to-wishlist cursor-pointer group"
         onClick={() => addToWishlist(product)}
-        children={
-          wishlist.some((el) => el.objectId === product.objectId)
-            ? "Remove from Wishlist"
-            : "Add to Wishlist"
-        }
-        twclass={"py-4 !px-6 !min-w-61"}
-      />
-      <BlackLineButton
-        onClick={() => addToCart(product)}
-        children={isOutOfStock ? "Out of stock" : "Add to Cart"}
-        twclass={
-          isOutOfStock
-            ? "!cursor-not-allowed !text-stone-500 !border-stone-500 hover:!bg-transparent hover:!text-stone-500 hover:!scale-100"
-            : "!py-4 !px-18"
-        }
-        disabled={isOutOfStock}
-      />
+      >
+        {isLiked ? (
+          <WishlistAdded cl=" w-[62px] h-[62px] bg-red-50 border-1 border-red-200 p-4 rounded-2xl transition-all duration-200 group-hover:scale-95" />
+        ) : (
+          <Wishlist cl=" w-[62px] h-[62px] border-1 border-black p-4 rounded-2xl transition-all duration-200  group-hover:scale-95 group-hover:border-red-200" />
+        )}
+      </button>
     </div>
   );
 }
