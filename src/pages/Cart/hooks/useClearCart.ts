83d442aad73598d776520ce7c6d7e2d55865dbb9
Yuriy <pe4enka055@gmail.com>
2025-10-15 12:15:32 +0300

    Updated logic; Added props - userId to mutationKey and queryKey

--- src/pages/Cart/hooks/useClearCart.ts
+++ src/pages/Cart/hooks/useClearCart.ts
@@ -1,21 +1,21 @@
-import { TCartItem } from "types/CartTypes";
-import DB_URL from "constants/DB_URL";
 import axios from "axios";
-import { useAuthStore } from "store/authStore";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-async function clearCart() {
-  const userId = useAuthStore.getState().user.firebaseId;
+import DB_URL from "constants/DB_URL";
+
+import { TCartItem } from "types/CartTypes";
+
+async function clearCart(userId: string) {
   await axios.delete<TCartItem[]>(`${DB_URL}/users/${userId}/cart.json`);
 }
 
-export default function useClearCart() {
+export default function useClearCart(userId: string) {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: clearCart,
-    mutationKey: ["cart"],
+    mutationFn: () => clearCart(userId),
+    mutationKey: ["cart", userId],
     onSuccess() {
-      queryClient.invalidateQueries({ queryKey: ["cart"] });
+      queryClient.invalidateQueries({ queryKey: ["cart", userId] });
     },
   });
 }
