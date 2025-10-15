dc3e5b60816e268af9cf9381fde4b3b19b99653c
Yuriy <pe4enka055@gmail.com>
2025-10-15 11:27:31 +0300

    Added fallbacks

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,5 +1,9 @@
+
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router";
+
+import MyLoader from "pages/Product/components/Params/components/Loader";
+import CartLoader from "pages/Cart/components/Loader";
 
 const Home = lazy(() => import("pages/Home"));
 const Catalog = lazy(() => import("pages/Catalog"));
@@ -11,18 +15,30 @@ const Wishlist = lazy(() => import("pages/Wishlist"));
 
 export default function RoutesComponent() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/catalog" element={<ChoosingCategories />} />
-        {/* <Route path='/contacts' element={<Contacts />} />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/catalog" element={<ChoosingCategories />} />
+      {/* <Route path='/contacts' element={<Contacts />} />
             <Route path='/blog' element={<Blog />} /> */}
-        <Route path="/catalog/:category" element={<Catalog />} />
-        <Route path="/catalog/:category/:fullname" element={<Product />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-      </Routes>
-    </Suspense>
+      <Route path="/catalog/:category" element={<Catalog />} />
+      <Route
+        path="/catalog/:category/:fullname"
+        element={
+          <Suspense fallback={<MyLoader />}>
+            <Product />
+          </Suspense>
+        }
+      />
+      <Route path="/profile" element={<Profile />} />
+      <Route
+        path="/cart"
+        element={
+          <Suspense fallback={<CartLoader className="mx-auto my-20 px-8" />}>
+            <Cart />
+          </Suspense>
+        }
+      />
+      <Route path="/wishlist" element={<Wishlist />} />
+    </Routes>
   );
 }
