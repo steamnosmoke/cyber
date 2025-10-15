3477273777bb9bc228ce416481e1b95c3fa77e78
Yuriy <pe4enka055@gmail.com>
2025-10-15 12:08:34 +0300

    Fixed bugs; Updated structure

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,25 +1,30 @@
 import { useEffect } from "react";
 
-import useGetItems from "hooks/useGetItems";
-import { TCartItem } from "types/CartTypes";
+import { useAuthStore } from "store/authStore";
+import useGetCart from "hooks/cart/useGetCart";
+
 import { useCartStore } from "./store/cartStore";
 
 import CartItem from "./components/CartItem";
 import EmptyCart from "./components/EmptyCart";
 import Summary from "./components/Summary";
+import Loader from "./components/Loader";
 
 export default function Cart() {
-  const { items: products, status } = useGetItems<TCartItem>("cart");
+  const userId = useAuthStore((state) => state.firebaseId);
+  const { cart: products, status } = useGetCart(userId);
+
   const calcNumbers = useCartStore((state) => state.calcNumbers);
   const count = useCartStore((state) => state.count);
 
   useEffect(() => {
-    calcNumbers(products);
+    calcNumbers(products || []);
   }, [calcNumbers, products]);
+
 
   return (
     <section className="cart py-4 px-0 flex-grow">
-      <div className="container">
+      <div className="container min-h-[calc(100vh-180px)]">
         <h1 className="mb-4 text-[32px] text-center font-medium">
           Shopping Cart
         </h1>
@@ -40,14 +45,12 @@ export default function Cart() {
           ) : (
             <EmptyCart />
           )
-        ) : status === "pending" ? (
-          <h2 className="text-4xl text-center font-semibold mt-[10%]">
-            Loading...
-          </h2>
-        ) : (
+        ) : status === "error" ? (
           <h2 className="text-4xl text-center font-semibold mt-[10%]">
             Error. Repeat later
           </h2>
+        ) : (
+          <Loader />
         )}
       </div>
     </section>
