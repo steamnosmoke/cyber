84fff4bac5bd96b9c1749179438c221d635cf6c5
Yuriy <pe4enka055@gmail.com>
2025-10-15 12:02:48 +0300

    Minor update

--- src/pages/Product/components/Params/components/Memory.tsx
+++ src/pages/Product/components/Params/components/Memory.tsx
@@ -2,7 +2,7 @@ import { useMemo } from "react";
 
 import { useProductStore } from "store/productsStore";
 
-import setItem from "utils/convertToCartItem";
+import setItem from "utils/cart/convertToCartItem";
 import memories from "../config/memories";
 
 export default function Memory() {
@@ -10,7 +10,7 @@ export default function Memory() {
 
   const memoizedMemories = useMemo(() => memories(product), [product]);
   return (
-    <ul className="memory_list flex items-center justify-between gap-4">
+    <ul className="memory_list flex items-center justify-between w-full">
       {memoizedMemories.map((mem, memIndex) => (
         <li
           className={`memory_item min-w-31 py-4 px-6 text-[14px] leading-4 text-center  border-1  rounded-[8px] transition-all duration-200 cursor-pointer hover:border-black hover:text-black ${
