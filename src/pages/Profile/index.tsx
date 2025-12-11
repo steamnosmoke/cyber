9520b2ce48b4bc3a2e0ff14a3825ea914a82d674
Yuriy <pe4enka055@gmail.com>
2025-12-11 13:45:32 +0300

    Updated using hooks and visual of component

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -2,24 +2,23 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
 import { useAuthStore } from "store/authStore";
+import useGetUserById from "hooks/useGetUserById";
 
 import Orders from "./components/Orders";
-import BlackButton from "buttons/components/BlackButton";
-import BlackLineButton from "buttons/components/BlackLineButton";
 import UserData from "./components/UserData";
 import { useChangeData } from "./components/UserData/store/useChageData";
-import { syncUserData } from "utils/syncUserData";
+import BlackButton from "buttons/components/BlackButton";
+import BlackLineButton from "buttons/components/BlackLineButton";
 
 export default function Profile() {
   const [isData, setIsData] = useState(false);
   const navigate = useNavigate();
-  const user = useAuthStore((state) => state.user);
+  const userId = useAuthStore((state) => state.firebaseId);
   const logOut = useAuthStore((state) => state.logOut);
   const clearData = useChangeData((state) => state.clearData);
+  const setUser = useChangeData((state) => state.setUser);
 
-  useEffect(() => {
-    syncUserData();
-  }, [user]);
+  const { data: user } = useGetUserById(userId);
 
   const onLogOut = () => {
     logOut();
@@ -27,28 +26,30 @@ export default function Profile() {
     navigate("/");
   };
 
-  if (user)
+  useEffect(() => {
+    setUser(user);
+  }, [user]);
+
+  if (userId !== "guest")
     return (
       <section className="flex-grow">
-        {user && (
-          <div className="container">
-            <div className="flex items-center justify-between py-10 px-0 border-b-1 border-b-stone-300">
-              <h1 className="hello text-3xl leading-9">
-                Hello, {user.name ? user.name : user.email}!
-              </h1>
-              <div className="buttons flex items-center gap-6">
-                <BlackLineButton
-                  children="Change your profile"
-                  twclass="!px-8"
-                  onClick={() => setIsData(!isData)}
-                />
-                <BlackButton children="Log out" onClick={onLogOut} />
-              </div>
+        <div className="container min-h-[calc(100vh-180px)]">
+          <div className="flex items-center justify-between py-10 px-0 border-b-1 border-b-stone-300">
+            <h1 className="hello text-3xl leading-9">
+              Hello, {user?.name ? user?.name : user?.email}!
+            </h1>
+            <div className="buttons flex items-center gap-6">
+              <BlackLineButton
+                children="Change your profile"
+                twclass="!px-8"
+                onClick={() => setIsData(!isData)}
+              />
+              <BlackButton children="Log out" onClick={onLogOut} />
             </div>
-            <UserData flag={isData} />
-            <Orders />
           </div>
-        )}
+          <UserData flag={isData} />
+          <Orders />
+        </div>
       </section>
     );
 }
