2461c51c140652a4ee76d95c1822d1fbf16185ce
Yuriy <pe4enka055@gmail.com>
2025-12-11 13:49:17 +0300

    Updated params for different categories

--- src/shared/components/ProductCard/config/params.ts
+++ src/shared/components/ProductCard/config/params.ts
@@ -2,9 +2,10 @@ import { TProduct } from "types/ProductTypes";
 
 export default function getParams(product: TProduct) {
   const category = product.category;
-  return category === "phone"
+  return category === "Phones"
     ? `${product.memory}${Number(product.memory) === 1 ? "TB" : "GB"}`
     : category === "Computers"
     ? `${product.storage}/${product.ram}`
-    : category === "Smartwatches" && `${product.size}`;
+    : category === "Smartwatches" ? `${product.size}` :
+    category === "Gaming Consoles" && `${product.storage}`;
 }
