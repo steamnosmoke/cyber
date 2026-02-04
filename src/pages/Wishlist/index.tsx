af8d0470d8a7e85fb4436d925705a8ece4fc4c27
Yuriy <pe4enka055@gmail.com>
2026-02-04 17:27:47 +0300

    Minor update

--- src/pages/Wishlist/index.tsx
+++ src/pages/Wishlist/index.tsx
@@ -5,11 +5,12 @@ import useGetWishlist from "hooks/wishlist/useGetWishlist";
 export default function Wishlist() {
   const userId = useAuthStore((state) => state.firebaseId);
   const { wishlist, status } = useGetWishlist(userId);
+
   return (
-    <section className="wishlist mt-4 flex-grow ">
+    <section className="wishlist mt-4 flex-grow">
       <div className="container min-h-[calc(100vh-120px)]">
-        <h1 className="text-3xl font-medium text-center mb-6">WishList</h1>
-        <Products products={wishlist} status={status} />
+        <h1 className="text-3xl font-medium text-center mb-4">WishList</h1>
+        <Products products={wishlist} status={status} grid={"start"} />
       </div>
     </section>
   );
